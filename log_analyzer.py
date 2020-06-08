fabc4f3f9c3704e9adbbdf2bbdc697010f4bea77
Alec Johnson <alecjohnson55@gmail.com>
2020-06-07 20:43:14 -0700

    throw valueerror on empty input

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,5 +1,8 @@
 class LogAnalyzer:
     def is_valid_log_filename(self, filename: str) -> bool:
+        if not filename:
+            raise ValueError("filename has to be provided")
+
         if not filename.upper().endswith(".SLF"):
             return False
         return True
