9198d0601dddef5b80204eb3be24938e820c0a26
Alec Johnson <alecjohnson55@gmail.com>
2020-06-07 19:13:06 -0700

    switch to pytest

--- tests/log_analyzer_test.py
+++ tests/log_analyzer_test.py
@@ -1,29 +1,24 @@
-import unittest
 from log_analyzer import LogAnalyzer
 
 
-class LogAnalyzerTests(unittest.TestCase):
+class TestLogAnalyzer:
     def test__is_valid_log_filename__bad_extension__returns_false(self):
         analyzer = LogAnalyzer()
 
         result = analyzer.is_valid_log_filename("filewithbadextension.foo")
 
-        self.assertFalse(result)
+        assert not result
 
     def test__is_valid_log_filename__good_extension_lowercase_returns_true(self):
         analyzer = LogAnalyzer()
 
         result = analyzer.is_valid_log_filename("filewithgoodextensionlower.slf")
 
-        self.assertTrue(result)
+        assert result
 
     def test__is_valid_log_filename__good_extension_uppercase_returns_true(self):
         analyzer = LogAnalyzer()
 
         result = analyzer.is_valid_log_filename("filewithgoodextensionupper.SLF")
 
-        self.assertTrue(result)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert result
