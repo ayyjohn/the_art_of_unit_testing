e7e445d31dba63cbfdba0d61d961c31d7a418c9f
Alec Johnson <alecjohnson55@gmail.com>
2020-06-08 09:30:38 -0700

    add getter and setter for last filename validity

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,8 +1,20 @@
 class LogAnalyzer:
+    def __init__(self):
+        self._was_last_filename_valid = False
+
     def is_valid_log_filename(self, filename: str) -> bool:
         if not filename:
             raise ValueError("filename has to be provided")
 
         if not filename.upper().endswith(".SLF"):
             return False
+
         return True
+
+    @property
+    def was_last_filename_valid(self):
+        return self._was_last_filename_valid
+
+    @was_last_filename_valid.setter
+    def was_last_filename_valid(self, value: bool):
+        self._was_last_filename_valid = value
