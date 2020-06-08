019718190769b2d122ca0ead4f4ab3e3d1741583
Alec Johnson <alecjohnson55@gmail.com>
2020-06-07 19:18:58 -0700

    lint

--- tests/log_analyzer_test.py
+++ tests/log_analyzer_test.py
@@ -10,8 +10,17 @@ class TestLogAnalyzer:
 
         assert not result
 
-    @pytest.mark.parametrize("input", ["filewithgoodextensionlower.slf", "filewithgoodextensionupper.SLF"])
-    def test__is_valid_log_filename__valid_extension__returns_true(self, input):
+    @pytest.mark.parametrize(
+        "input",
+        [
+            "filewithgoodextensionlower.slf",
+            "filewithgoodextensionupper.SLF",
+        ]
+    )
+    def test__is_valid_log_filename__valid_extension__returns_true(
+        self,
+        input
+    ):
         analyzer = LogAnalyzer()
 
         result = analyzer.is_valid_log_filename(input)
