4b1fe8814013f3977a9f202c667704a7fda9da08
Alec Johnson <alecjohnson55@gmail.com>
2020-06-08 21:02:28 -0700

    add black formatting

--- tests/log_analyzer_test.py
+++ tests/log_analyzer_test.py
@@ -1,4 +1,5 @@
 import pytest
+
 from log_analyzer import LogAnalyzer
 
 
@@ -11,16 +12,9 @@ class TestLogAnalyzer:
         assert not result
 
     @pytest.mark.parametrize(
-        "input",
-        [
-            "filewithgoodextensionlower.slf",
-            "filewithgoodextensionupper.SLF",
-        ]
+        "input", ["filewithgoodextensionlower.slf", "filewithgoodextensionupper.SLF", ]
     )
-    def test__is_valid_log_filename__valid_extension__returns_true(
-        self,
-        input
-    ):
+    def test__is_valid_log_filename__valid_extension__returns_true(self, input: str):
         analyzer = LogAnalyzer()
 
         result = analyzer.is_valid_log_filename(input)
@@ -28,25 +22,17 @@ class TestLogAnalyzer:
         assert result
 
     @pytest.mark.exceptions
-    def test__is_valid_log_filename__empty_filename__throws_argument_exception(
-        self
-    ):
+    def test__is_valid_log_filename__empty_filename__throws_argument_exception(self):
         analyzer = LogAnalyzer()
 
         with pytest.raises(ValueError):
             analyzer.is_valid_log_filename("")
 
     @pytest.mark.parametrize(
-        "input,expected",
-        [
-            ("invalidname.foo", False),
-            ("validname.slf", True),
-        ]
+        "input,expected", [("invalidname.foo", False), ("validname.slf", True), ]
     )
     def test__is_valid_log_filename__when_called__changes_was_last_filename_valid(
-        self,
-        input: str,
-        expected: bool
+            self, input: str, expected: bool
     ):
         analyzer = LogAnalyzer()
 
