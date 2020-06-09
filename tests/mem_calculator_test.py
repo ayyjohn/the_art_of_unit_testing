79a6e650561f56fd7c84b94a72558e0ba41e48d8
Alec Johnson <alecjohnson55@gmail.com>
2020-06-08 21:21:21 -0700

    use a factory method for calculator

--- tests/mem_calculator_test.py
+++ tests/mem_calculator_test.py
@@ -5,16 +5,19 @@ from mem_calculator import MemCalculator
 
 class TestMemCalculator:
     def test__sum__by_default__returns_zero(self):
-        calculator = MemCalculator()
+        calculator = self._make_calculator()
 
         last_total = calculator.total
 
         assert last_total == 0
 
     def test__add__when_called__changes_sum(self):
-        calculator = MemCalculator()
+        calculator = self._make_calculator()
 
         calculator.add(1)
         total = calculator.total
 
         assert total == 1
+
+    def _make_calculator(self):
+        return MemCalculator()
