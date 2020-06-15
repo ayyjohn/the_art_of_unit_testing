79e49b88d3c626a991b06d4e51784c6ada367df3
Alec Johnson <alecjohnson55@gmail.com>
2020-06-14 21:14:56 -0700

    allow for injecting file_extension_manager in log_analyzer

--- log_analyzer.py
+++ log_analyzer.py
@@ -2,14 +2,14 @@ from file_extension_manager import FileExtensionManager
 
 
 class LogAnalyzer:
-    def __init__(self):
+    def __init__(self, file_extension_manager: FileExtensionManager):
         self._was_last_filename_valid = False
+        self.file_extension_manager = file_extension_manager
 
     def is_valid_log_filename(self, filename: str) -> bool:
-        manager = FileExtensionManager()
         self._was_last_filename_valid = False
 
-        result = manager.is_valid(filename)
+        result = self.file_extension_manager.is_valid(filename)
         self.was_last_filename_valid = result
 
         return result
