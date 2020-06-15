156041feb895f433c5667337a812de92894568fd
Alec Johnson <alecjohnson55@gmail.com>
2020-06-14 19:57:01 -0700

    refactor input to input_ in tests

--- tests/log_analyzer_test.py
+++ tests/log_analyzer_test.py
@@ -12,12 +12,12 @@ class TestLogAnalyzer:
         assert not result
 
     @pytest.mark.parametrize(
-        "input", ["filewithgoodextensionlower.slf", "filewithgoodextensionupper.SLF", ]
+        "input_", ["filewithgoodextensionlower.slf", "filewithgoodextensionupper.SLF", ]
     )
-    def test__is_valid_log_filename__valid_extension__returns_true(self, input: str):
+    def test__is_valid_log_filename__valid_extension__returns_true(self, input_: str):
         analyzer = self._make_log_analyzer()
 
-        result = analyzer.is_valid_log_filename(input)
+        result = analyzer.is_valid_log_filename(input_)
 
         assert result
 
@@ -29,14 +29,14 @@ class TestLogAnalyzer:
             analyzer.is_valid_log_filename("")
 
     @pytest.mark.parametrize(
-        "input,expected", [("invalidname.foo", False), ("validname.slf", True), ]
+        "input_,expected", [("invalidname.foo", False), ("validname.slf", True), ]
     )
     def test__is_valid_log_filename__when_called__changes_was_last_filename_valid(
-            self, input: str, expected: bool
+            self, input_: str, expected: bool
     ):
         analyzer = self._make_log_analyzer()
 
-        analyzer.is_valid_log_filename(input)
+        analyzer.is_valid_log_filename(input_)
 
         assert analyzer.was_last_filename_valid == expected
 
