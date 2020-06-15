3fd18d80ebe13ad4eb2063d10002d824efcf5fec
Alec Johnson <alecjohnson55@gmail.com>
2020-06-14 20:17:14 -0700

    remove abstract get valid extension method

--- file_extension_manager.py
+++ file_extension_manager.py
@@ -6,7 +6,3 @@ class FileExtensionManager(ABC):
     @abstractmethod
     def is_valid(self, filename: str) -> bool:
         raise NotImplementedError
-
-    @abstractmethod
-    def get_valid_extension(self) -> str:
-        raise NotImplementedError
