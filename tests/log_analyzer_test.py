6c6e9b0b999fc92754df7009021df03a1621c0f0
Alec Johnson <alecjohnson55@gmail.com>
2020-06-16 20:54:50 -0700

    rename make_log_analyzer methods and log_analyzer

--- tests/log_analyzer_test.py
+++ tests/log_analyzer_test.py
@@ -6,9 +6,9 @@ from fake_file_extension_manager import FakeFileExtensionManager
 
 class TestLogAnalyzer:
     def test__is_valid_log_filename__bad_extension__returns_false(self):
-        analyzer = _make_invalid_log_analyzer()
+        log_analyzer = _make_log_analyzer_with_invalid_file_extension_manager()
 
-        result = analyzer.is_valid_log_filename("filewithbadextension.foo")
+        result = log_analyzer.is_valid_log_filename("filewithbadextension.foo")
 
         assert not result
 
@@ -16,18 +16,18 @@ class TestLogAnalyzer:
         "input_", ["filewithgoodextensionlower.slf", "filewithgoodextensionupper.SLF", ]
     )
     def test__is_valid_log_filename__valid_extension__returns_true(self, input_: str):
-        analyzer = _make_valid_log_analyzer()
+        log_analyzer = _make_log_analyzer_with_valid_file_extension_manager()
 
-        result = analyzer.is_valid_log_filename(input_)
+        result = log_analyzer.is_valid_log_filename(input_)
 
         assert result
 
     @pytest.mark.exceptions
     def test__is_valid_log_filename__empty_filename__throws_argument_exception(self):
-        analyzer = _make_valid_log_analyzer()
+        log_analyzer = _make_log_analyzer_with_valid_file_extension_manager()
 
         with pytest.raises(ValueError):
-            analyzer.is_valid_log_filename("")
+            log_analyzer.is_valid_log_filename("")
 
     @pytest.mark.parametrize(
         "input_,expected", [("invalidname.foo", False), ("validname.slf", True), ]
@@ -47,7 +47,7 @@ def _make_log_analyzer():
     return LogAnalyzer()
 
 
-def _make_valid_log_analyzer():
+def _make_log_analyzer_with_valid_file_extension_manager():
     always_valid_fake_file_extension_manager = FakeFileExtensionManager()
     always_valid_fake_file_extension_manager.will_be_valid = True
     log_analyzer = LogAnalyzer()
@@ -55,7 +55,7 @@ def _make_valid_log_analyzer():
     return log_analyzer
 
 
-def _make_invalid_log_analyzer():
+def _make_log_analyzer_with_invalid_file_extension_manager():
     always_invalid_fake_file_extension_manager = FakeFileExtensionManager()
     always_invalid_fake_file_extension_manager.will_be_valid = False
     log_analyzer = LogAnalyzer()
