87717cef5bcaea83a48902e11357c23967c8a4e7
Alec Johnson <alecjohnson55@gmail.com>
2020-06-17 08:49:54 -0700

    add a file extension manager factory

---
+++ file_extension_manager_factory.py
@@ -0,0 +1,11 @@
+from text_config_based_file_extension_manager import TextConfigBasedFileExtensionManager
+
+
+class FileExtensionManagerFactory:
+    def __init__(self):
+        self.custom_manager = None
+
+    def create(self):
+        if self.custom_manager is not None:
+            return self.custom_manager
+        return TextConfigBasedFileExtensionManager()
